8b0e3f5e783c4ae912c6eca2493e75b205b79338
mewbaeru <klyukina.anzhelika@mail.ru>
2025-06-29 15:35:33 +0300

    feat(main.py): launching the application

---
+++ main.py
@@ -0,0 +1,10 @@
+from PyQt5 import QtWidgets
+
+from window.main_window import MainWindow
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_()) 
