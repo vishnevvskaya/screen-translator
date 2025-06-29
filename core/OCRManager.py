d8480077ee7783dea6f58a55b8be54f86059f445
mewbaeru <klyukina.anzhelika@mail.ru>
2025-06-29 15:54:55 +0300

    feat(OCRManager.py): implement periodic text recognition with Qt threads

---
+++ core/OCRManager.py
@@ -0,0 +1,30 @@
+from PyQt5 import QtCore
+
+from modules import *
+
+class OCRManager:
+    def __init__(self):
+        self.recognition_thread = None
+        self.recognition_timer = QtCore.QTimer()
+        self.recognition_timer.timeout.connect(self.recognize_text)
+    
+    def setup(self, interval=5000):
+        '''Setting up periodic recognition'''
+        self.recognition_timer.start(interval)
+    
+    def recognize_text(self):
+        '''Performing text recognition'''
+        if self.recognition_thread and self.recognition_thread.isRunning():
+            self.recognition_thread.stop()
+        
+        text_recognition = TextRecognition('English')
+        self.recognition_thread = RecognitionThread(self.selection, text_recognition)
+        self.recognition_thread.text_recognized.connect(lambda text: (self.window.text_area.setText(text), print(text)))
+        self.recognition_thread.start()
+    
+    def close(self):
+        '''Resource cleaning'''
+        self.recognition_timer.stop()
+        if self.recognition_thread:
+            self.recognition_thread.stop()
+        self.window.close()
