dd6c8db9abef649cb44bfa79f116ae095508ef7f
vishnevvskaya <klyukina.anzhelika@mail.ru>
2026-02-26 22:46:02 +0300

    feat(download_models.py): initialize PaddleOCR with custom model directory

---
+++ download_models.py
@@ -0,0 +1,14 @@
+from paddleocr import PaddleOCR
+import os
+
+os.makedirs('paddle_models', exist_ok=True)
+
+model = PaddleOCR(
+    lang='en',
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False,
+    use_textline_orientation=False,
+    device='gpu', 
+)
+
+print("Models successfully downloaded!")
