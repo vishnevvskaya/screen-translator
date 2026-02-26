9a8381ce409f09ce5f0e83c06a5e5d4bcd124c3b
vishnevvskaya <klyukina.anzhelika@mail.ru>
2026-02-26 23:51:20 +0300

    feat(main.py): add app icon

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 
 from window.main_window import MainWindow
 
 if __name__ == "__main__":
+    import ctypes
     import sys
-    app = QtWidgets.QApplication(sys.argv)
+
+    if sys.platform == "win32":
+        myappid = 'screen.translator.app.1.0'
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('icon.png'))
     window = MainWindow()
     window.show()
     sys.exit(app.exec_()) 
